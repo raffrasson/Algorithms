57c480b43c74c4fbf829a3d5d82bb161349d06c3
raffrasson <raf.frasson@gmail.com>
2022-10-18 10:47:49 -0300

    fix: fixes flake8 errors

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,16 +1,10 @@
 def is_anagram(first_string, second_string):
     def sorter(list):
-        n = len(list)  
+        n = len(list)
 
-        for ordered_elements in range(
-            n - 1
-        ):  
-            for item in range(
-                0, n - 1 - ordered_elements
-            ):  
-                if (
-                    list[item] > list[item + 1]
-                ):  
+        for ordered_elements in range(n - 1):
+            for item in range(0, n - 1 - ordered_elements):
+                if list[item] > list[item + 1]:
                     current_element = list[item]
                     list[item] = list[item + 1]
                     list[item + 1] = current_element
@@ -20,10 +14,8 @@ def is_anagram(first_string, second_string):
     first_string_formatted = list(first_string.lower())
     second_string_formatted = list(second_string.lower())
 
-
     first_string_ordered = sorter(first_string_formatted)
     second_string_ordered = sorter(second_string_formatted)
-
 
     if first_string_ordered == second_string_ordered:
         return True
