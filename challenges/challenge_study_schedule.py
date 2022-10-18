a5ba7304b302ad3d75ef05ae5292da9e0a535a0b
raffrasson <raf.frasson@gmail.com>
2022-10-17 21:24:55 -0300

    fix: fixes flake8 errors

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -2,13 +2,7 @@ def study_schedule(permanence_period, target_time):
     students = 0
     try:
         for entry, leave in permanence_period:
-            if entry == None:
-                return None
-            if leave == None:
-                return None
-            if target_time == None:
-                return None
-            if entry <= leave and entry <= target_time and leave >= target_time:
+            if entry <= leave and entry <= target_time <= leave:
                 students += 1
         return students
     except TypeError:
